a8e8298534bb05432940faf6b5ba8cb9bb070432
Othman <oassas@MacBook-Pro-de-assas.local>
2023-05-08 13:05:31 +0200

    end factory pattern

--- 04_factory_pattern/index.js
+++ 04_factory_pattern/index.js
@@ -1,3 +1,8 @@
+const TYPES = {
+    event : "event",
+    todo: "todo"
+}
+
 class Task {
     constructor(name) {
         this.name = name;
@@ -34,12 +39,55 @@ class EventTask extends Task {
         console.log("This event is scheduled on: " + new Date(this.scheduleDate).toLocaleDateString())
     }
 }
+
+class TodoTask extends Task {
+    constructor(name) {
+        super(name)
+        this.priorities = ["normal", "high"];
+        this.priority = 0;
+    }
+    makeUrgent() {
+        this.priority = !this.priority
+    }
+    toString() {
+        if (this.priority) {
+            console.log("High priority task: " + this.name);
+            return
+        }
+        console.log("Task: " + this.name)
+    }
+}
+
+class TaskFactory {
+    createTask(type, name){
+        switch (type) {
+            case TYPES.event:
+                return new EventTask(name)
+            case TYPES.todo:
+                return new TodoTask(name)
+        }
+    }
+}
+
 const task1 = new Task("learn javascript design patterns");
 const task2 = new Task("learn flutter");
 const ev = new EventTask("learn javascript design patterns");
+const todo = new TodoTask("create new class JS")
 
 task1.toString();
 task2.toString();
 task1.complete();
 task1.toString();
 ev.toString();
+todo.toString();
+todo.makeUrgent();
+todo.toString();
+
+// factory
+console.log("=== factory ===")
+const factory = new TaskFactory();
+const evt1 = factory.createTask(TYPES.event, " event learn factory pattern");
+evt1.toString()
+const newTask = factory.createTask(TYPES.todo, "pay bills");
+newTask.makeUrgent()
+newTask.toString()
